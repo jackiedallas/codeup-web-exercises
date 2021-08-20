d365f0b86aee3c0b6d09717a885f662db945a866
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-08-20 09:13:52 -0500

    arrays lecture

--- js/practice.js
+++ js/practice.js
@@ -1,17 +1,18 @@
-'use strict'
+// 'use strict'
+//
+// console.log("Ice Cream Seller question...");
+// console.log('');
+// // Still figuring this one out...
+// function iceCreamSeller(x, y) {
+// 	// var conesToSell = Math.floor(Math.random() * 50) + 50;
+// 	// var conesBought = Math.floor(Math.random() * 5) + 1;
+// 	do{
+// 		if (true) {
+// 			var conesBought = x - y;
+// 			console.log("I just sold " + conesBought + " ice cream cones.")
+// 			x--;
+// 		}
+// 	} while(x >= 8)
+// }
+// iceCreamSeller(10, 2);
 
-console.log("Ice Cream Seller question...");
-console.log('');
-// Still figuring this one out...
-function iceCreamSeller(x, y) {
-	// var conesToSell = Math.floor(Math.random() * 50) + 50;
-	// var conesBought = Math.floor(Math.random() * 5) + 1;
-	do{
-		if (true) {
-			var conesBought = x - y;
-			console.log("I just sold " + conesBought + " ice cream cones.")
-			x--;
-		}
-	} while(x >= 8)
-}
-iceCreamSeller(10, 2);
