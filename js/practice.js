0790101e8458f6259d8a07ee4ff41b632126f97c
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-09-07 16:01:54 -0500

    started flexbox

--- js/practice.js
+++ js/practice.js
@@ -193,3 +193,34 @@ return re;
 // 		button.innerHTML = ++i
 // 	}
 // })
+
+function factorial (number) {
+	 var product = 1;
+	for (let i = 1; i <=number ; i++) {
+		product *= i;
+	}
+	return product;
+}
+
+console.log(factorial(8));
+
+function getGrade(score) {
+	let grade;
+	// Write your code here
+	if(score > 25) {
+		grade = 'A';
+	} else if (score > 20) {
+		grade = 'B';
+	} else if (score > 15) {
+		grade = 'C';
+	} else if (score > 10) {
+		grade = 'D';
+	} else if (score > 5) {
+		grade = 'E';
+	} else {
+		return 'F';
+	}
+	return grade;
+}
+
+console.log(getGrade(17));
