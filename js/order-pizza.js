0fe5bb58ea374900f0d9a61744d7467df283b632
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-09-13 17:05:21 -0500

    added the rest of the tab content and buttons

---
+++ js/order-pizza.js
@@ -0,0 +1,14 @@
+$('#nav-sizeCrust a').on('click', function (e) {
+	e.preventDefault()
+	$(this).tab('show')
+})
+
+$('#nav-cheeseSauce a').on('click', function (e) {
+	e.preventDefault()
+	$(this).tab('show')
+})
+
+$('#nav-topping a').on('click', function (e) {
+	e.preventDefault()
+	$(this).tab('show')
+})
