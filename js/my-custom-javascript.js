fc97d9e7687af6103ef15627f3d684584eb2bf1e
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-09-27 22:08:38 -0500

    completed reading for selectors module of jquery

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,21 @@
 "use strict"
 
-$(document).ready(function () {
-	alert("DOM has finished loading")
-})
+// $(document).ready(function () {
+// 	alert("DOM has finished loading");
+// })
+
+// USING THE ID SELECTOR
+// var contents = $('#header1').html();
+// alert(contents);
+
+// USING THE CLASS SELECTOR
+$('.important').css('background-color', 'red');
+
+// USING THE ELEMENT SELECTOR
+$('p').css('font-size', '14px');
+
+// USING THE MULTIPLE SELECTOR
+$('.important, p').css('background-color', '#ff0');
+
+// USING THE ALL SELECTOR
+$('*').css('border', '1px solid #f00');
