23fe65858deb17cba13528d63061bb476eb40b18
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-09-27 22:48:18 -0500

    finished first exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,22 +1,37 @@
 "use strict"
 
-// $(document).ready(function () {
-// 	alert("DOM has finished loading");
-// })
+// // $(document).ready(function () {
+// // 	alert("DOM has finished loading");
+// // })
+//
+// // USING THE ID SELECTOR
+// // var contents = $('#header1').html();
+// // alert(contents);
+//
+// // USING THE CLASS SELECTOR
+// $('.important').css('background-color', 'red');
+//
+// // USING THE ELEMENT SELECTOR
+// $('p').css('font-size', '14px');
+//
+// // USING THE MULTIPLE SELECTOR
+// $('.important, p').css('background-color', '#ff0');
+//
+// // USING THE ALL SELECTOR
+// $('*').css('border', '1px solid #f00');
+//
 
-// USING THE ID SELECTOR
-// var contents = $('#header1').html();
-// alert(contents);
+// // CODE FOR JQUERY EXERCISE BELOW
 
-// USING THE CLASS SELECTOR
-$('.important').css('background-color', 'red');
+// ID Selector exercises
+// var test = $('#listItem1').html();
+// alert(test);
 
-// USING THE ELEMENT SELECTOR
-$('p').css('font-size', '14px');
+// Class Selector Exercises
+// $('.codeup').css('border', '1px solid red')
 
-// USING THE MULTIPLE SELECTOR
-$('.important, p').css('background-color', '#ff0');
-
-// USING THE ALL SELECTOR
-$('*').css('border', '1px solid #f00');
-
+// Element Selector Exercises
+$('li').css('font-size', '20px');
+$('h1, p, li').css('background-color', 'yellow');
+var test2 = $('h1').html();
+alert(test2);
