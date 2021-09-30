53b23f792cb80b5be8416d52a152ba767122e1b9
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-09-30 16:55:15 -0500

    highlighted each last child of list items

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,15 +1,26 @@
 "use strict"
 
 $(document).ready(function () {
-	$('.btn').css('background', 'none').css('color', 'blue').css('border', 'none')
+	$('body').css('color', 'limegreen').css('background-color', 'black')
 	$('dd').addClass('invisible');
+	// $('ul').addClass('list-facts')
 	// $('.invisible').css('visibility', 'hidden');
+	var highlightFacts = document.getElementById('highlight');
 	var showFacts = document.getElementById('showFacts');
+	
 	showFacts.addEventListener("click", function (e) {
 		e.preventDefault();
 		$('dd').toggleClass('invisible');
 		// $('.invisible').css('visibility', 'visible');
 	})
+	
+	highlightFacts.addEventListener('click', function (e){
+		e.preventDefault();
+		$('li:last-child').css('background-color', 'yellow');
+		// $('li').children().last().css('background-color', 'yellow');
+	})
+	
+	
 })
 
 
