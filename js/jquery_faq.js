38da242b01c095488c2a9ef5b84e280cfa66dca0
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-09-30 23:59:57 -0500

    created function to change color of all first li elements to blue

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -2,28 +2,31 @@
 
 $(document).ready(function () {
 	$('body').css('color', 'limegreen').css('background-color', 'black')
+	
+	// Add a class to all dd elements named invisible.
 	$('dd').addClass('invisible');
 	$('li:last-child').addClass('highlight')
-	// $('.highlight').css('background-color', 'yellow')
 	
-	// $('ul').addClass('list-facts')
-	// $('.invisible').css('visibility', 'hidden');
+	
 	var highlightFacts = document.getElementById('highlight');
 	var showFacts = document.getElementById('showFacts');
 	
+	
+	// Update the page with jQuery to add a link that toggles the class invisible on and off for all dd elements.
 	showFacts.addEventListener("click", function (e) {
 		e.preventDefault();
 		$('dd').toggleClass('invisible');
-		// $('.invisible').css('visibility', 'visible');
 	})
 	
+	
+	// Create a button that, when clicked, makes the last li in each ul have a yellow background.
 	highlightFacts.addEventListener('click', function (e){
 		e.preventDefault();
 		$('li:last-child').css('background-color', 'yellow');
 		// $('.highlight').toggleClass('highlight');
 	})
 	
-	
+	// When any h3 is clicked, the lis underneath it should be bolded. Use font-weight: bold to achieve this.
 	$('#earth-header').click(function () {
 		$('#earth-header').next().css('font-weight', 'bold')
 	})
@@ -34,6 +37,17 @@ $(document).ready(function () {
 		$('#saturn-header').next().css('font-weight', 'bold')
 	})
 	
+	
+	// When any list item is clicked, first li of the parent ul should have a font color of blue.
+	$('#earth-facts').click(function () {
+		$('#earth-facts li').first().css('color', 'blue')
+	})
+	$('#mars-facts').click(function () {
+		$('#mars-facts li').first().css('color', 'blue')
+	})
+	$('#saturn-facts').click(function () {
+		$('#saturn-facts li').first().css('color', 'blue')
+	})
 })
 
 
