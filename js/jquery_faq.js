c08ecbc200292ef1deea4e83030a5e453ef2cb3f
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-01 01:12:41 -0500

    added divs with photos in javascript to do bonus

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -3,6 +3,15 @@
 $(document).ready(function () {
 	$('body').css('color', 'limegreen').css('background-color', 'black')
 	
+	// add class to picture div & button
+	$('button').addClass('btn btn-light bg-light')
+	$('div').addClass('frame');
+	$('.frame').css({'width': '250px', 'height': '250px', 'border': '10px solid white', 'margin': '1em'});
+	$('#picture1').css({'background-image': 'url(/img/small-loner.jpg)', 'background-size': 'cover'})
+	$('#picture2').css({'background-image': 'url(/img/small-moon.jpg)', 'background-size': 'cover'})
+	$('#picture3').css({'background-image': 'url(/img/small-sunset.jpg)', 'background-size': 'cover'})
+	
+	
 	// Add a class to all dd elements named invisible.
 	$('dd').addClass('invisible');
 	$('li:last-child').addClass('highlight')
@@ -48,6 +57,22 @@ $(document).ready(function () {
 	$('#saturn-facts').click(function () {
 		$('#saturn-facts li').first().css('color', 'blue')
 	})
+	
+	//	Create 3 divs that each look like a picture frame. Each one should have a unique background image and a button underneath that swaps the image from the frame. Use the traversing functions of jQuery to accomplish this.
+	//
+	// The rules are the following:
+	//
+	// The left frame swaps to the right and takes the image from the frame in the center.
+	// The center frame swaps randomly to either the left or right.
+	// The right frame swaps to the left and takes the image from the frame in the center.
+	
+	$('#s-btn1').click(function (e) {
+		e.preventDefault();
+		// $('#picturesList li:eq(1)').before($('#picturesList li:eq(0)'));
+		
+		
+	})
+	
 })
 
 
