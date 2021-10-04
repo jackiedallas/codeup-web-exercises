d42c45fd6d7d1aad62e681d2be8c513539649162
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-04 10:59:27 -0500

    created mapbox accounted and added token to javascript file

---
+++ js/mapbox_maps_api.js
@@ -0,0 +1,15 @@
+"use strict"
+$(document).ready(function () {
+	mapboxgl.accessToken = pk.eyJ1IjoiamFja2llZGFsbGFzIiwiYSI6ImNrdWN1NmEwbjE0MTMyeG84Y3pmMnN6encifQ.pTLanvGV47qgP0FzumCIOw;
+	var map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v9',
+		zoom: 10,
+		center: [-98.4916, 29.4252]
+	})
+
+
+
+
+
+})
