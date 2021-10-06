b926df27bdf95f4c1629c5365f2844d84af1a2e0
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-06 11:35:43 -0500

    created get request for weathermap data and drew map with mapbox api on to html

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,5 +1,25 @@
 "use strict"
 
 $(document).ready(function () {
+	// Get Weather Data
+	$.get("http://api.openweathermap.org/data/2.5/weather", {
+		APPID: weatherMapKey,
+		q: "San Antonio, Texas",
+		units: 'imperial'
+	}).done(function (weatherData) {
+		console.log(weatherData)
+		// Create Map
+		mapboxgl.accessToken = mapBoxKey;
+		var weatherMap = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v11',
+			zoom: 10,
+			center: [-98.4861, 29.4252]
+		})
+		
+		
+	})
+	
+	
 	
 })
