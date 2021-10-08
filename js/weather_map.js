96b37a0f6aa2655ca5c622ca1f3aa143b77c6e4e
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-08 15:21:58 -0500

    added marker function, cards now update when you move marker to a new location

--- js/weather_map.js
+++ js/weather_map.js
@@ -3,21 +3,112 @@
 $(document).ready(function () {
 	$('#current-weather').click(function (event) { // using a click event to get a lat/long from user input for 5-day forecast
 		event.preventDefault();
-		var latitude = '';
-		var longitude = '';
+		// var latitude = '';
+		// var longitude = '';
+		$('#menu').css('visibility', 'visible')
 		geocode($('#location-search').val(), mapBoxKey).then(function (results) { // used geocode function to convert user input to lat/long coordinates
-			// console.log(results)
-			for (let i = 0; i < results.length; i++) {
-				if (i === 0) {
-					latitude += results[i];
-				} else {
-					longitude += results[i];
-				} // stored lat & long data into separate variables to pass to get request for OpenWeatherAPI
+			console.log(results)
+			// Create Map
+			mapboxgl.accessToken = mapBoxKey;
+			var weatherMap = new mapboxgl.Map({
+				container: 'map',
+				style: 'mapbox://styles/mapbox/streets-v11',
+				zoom: 10,
+				center: [results[0], results[1]]
+			})
+			
+			
+			// drag marker functionality
+			var locationMarker = new mapboxgl.Marker({
+				draggable: true,
+				color: 'red'
+			})
+
+				
+				.setLngLat(results)
+				.addTo(weatherMap)
+				.on('dragend', () => {
+					var results = locationMarker.getLngLat()
+					var coordArr = Object.values(results)
+
+					$.get('https://api.openweathermap.org/data/2.5/onecall', {
+						appid: weatherMapKey,
+						lat: coordArr[1], // came from geocode function & user input
+						lon: coordArr[0], // came from geocode function & user input
+						units: 'imperial'
+					})
+						.done(function (weather) {
+							console.log(weather)
+							var dailyWx = weather.daily; // stored daily weather data in variable to iterate easier
+							// console.log(dailyWx);
+							var userLocation = $('#location-search').val();
+							var cards = '';
+							for (let i = 0; i < dailyWx.length ; i++) {
+								if (i <= 4) {
+									// variable declarations for dates and times
+									let dt = new Date(dailyWx[i].dt * 1000).toLocaleDateString('en-US',{
+										weekday: 'short',
+										day: 'numeric',
+										year: 'numeric',
+										month: 'long'
+									});
+									let sr = new Date(dailyWx[i].sunrise * 1000).toLocaleTimeString();
+									let ss = new Date(dailyWx[i].sunset * 1000).toLocaleTimeString();
+									let precipitation = dailyWx[i].pop * 100;
+
+
+									// data for html
+									cards += '<div class="card-deck card-deck-font pt-3">' +
+										'<div class="card mx-5" id="cards">' +
+										'<div class="card-header text-center">' + '<h5>' + dt + '</h5>' + '</div>' +
+										'<div class="card-body">' +
+										'<h3 class="card-title text-center">' + dailyWx[i].weather[0].description + '</h3>' +
+										'<p class="card-text">' + '<span class="cardText">' + 'High: ' + '</span>' + dailyWx[i].temp.max.toFixed(1) + '&deg' + 'F' + '<span class="cardText">' + ' Low: ' + '</span>' +                                         dailyWx[i].temp.min.toFixed(1) + '&deg' + 'F' + '</p>' +
+										'<p class="card-text">' + '<span class="">' + 'High Feels Like  ' + '</span>' + dailyWx[i].feels_like.day.toFixed(1) + '&deg' + 'F' + '</p>' +
+										'<p class="card-text">' + '<span class="cardText">' + 'Humidity: ' + '</span>' + dailyWx[i].humidity + '%' + '</p>' +
+										'<p class="card-text">' + '<span class="cardText">' + 'UV Index: ' + '</span>' + dailyWx[i].uvi.toFixed(2) + '</p>' +
+										'<p class="card-text">' + '<span class="cardText">' + 'Wind Speed: ' + '</span>' + dailyWx[i].wind_speed + 'm/s, ' + dailyWx[i].wind_deg + '&deg' + '</p>' +
+										'<p class="card-text">' + '<span class="cardText">' + 'Precipitation: ' + '</span>' + parseInt(precipitation) + '%' + '</p>' +
+										'<p class="card-text">' + '<span class="cardText">' + 'Pressure: ' + '</span>' + dailyWx[i].pressure + 'mb' + '</p>' +
+										'<p class="card-text">' + '<span class="cardText">' + 'Sunrise: ' + '</span>' + sr + '</p>' +
+										'<p class="card-text">' + '<span class="cardText">' + 'Sunset: ' + '</span>' + ss + '</p>' +
+										'</div>' +
+										'</div>' +
+										'</div>'
+								}
+								$('#city-header').html('<h2 class="text-center">' + userLocation + '</h2>');
+								$('#forecastContainer').html(cards) // insert into html here
+
+
+
+
+							}
+						})
+					// alert('you moved the marker')
+				})
+			
+			// locationMarker.flyTo(results);
+			
+			
+			
+			
+			// change map style
+			
+			const layerList = document.getElementById('menu');
+			const inputs = layerList.getElementsByTagName('input');
+			
+			for (const input of inputs) {
+				input.onclick = (layer) => {
+					const layerId = layer.target.id;
+					weatherMap.setStyle('mapbox://styles/mapbox/' + layerId);
+				};
 			}
+			
+			// get weather data
 			$.get('https://api.openweathermap.org/data/2.5/onecall', {
 				appid: weatherMapKey,
-				lat: longitude, // came from geocode function & user input
-				lon: latitude, // came from geocode function & user input
+				lat: results[1], // came from geocode function & user input
+				lon: results[0], // came from geocode function & user input
 				units: 'imperial'
 			})
 				.done(function (weather) {
@@ -41,12 +132,12 @@ $(document).ready(function () {
 							
 							
 							// data for html
-							cards += '<div class="card-deck pt-3">' +
+							cards += '<div class="card-deck card-deck-font pt-3">' +
 								'<div class="card mx-5" id="cards">' +
 								'<div class="card-header text-center">' + '<h5>' + dt + '</h5>' + '</div>' +
 								'<div class="card-body">' +
 								'<h3 class="card-title text-center">' + dailyWx[i].weather[0].description + '</h3>' +
-								'<p class="card-text">' + '<span class="cardText">' + 'High: ' + '</span>' + dailyWx[i].temp.max.toFixed(1) + '&deg' + 'F' + '<span class="cardText">' + ' Low: ' + '</span>' + dailyWx[i].temp.min.toFixed(1) + '&deg' +                                 'F' + '</p>' +
+								'<p class="card-text">' + '<span class="cardText">' + 'High: ' + '</span>' + dailyWx[i].temp.max.toFixed(1) + '&deg' + 'F' + '<span class="cardText">' + ' Low: ' + '</span>' +                                         dailyWx[i].temp.min.toFixed(1) + '&deg' + 'F' + '</p>' +
 								'<p class="card-text">' + '<span class="">' + 'High Feels Like  ' + '</span>' + dailyWx[i].feels_like.day.toFixed(1) + '&deg' + 'F' + '</p>' +
 								'<p class="card-text">' + '<span class="cardText">' + 'Humidity: ' + '</span>' + dailyWx[i].humidity + '%' + '</p>' +
 								'<p class="card-text">' + '<span class="cardText">' + 'UV Index: ' + '</span>' + dailyWx[i].uvi.toFixed(2) + '</p>' +
@@ -63,14 +154,7 @@ $(document).ready(function () {
 						$('#forecastContainer').html(cards) // insert into html here
 						
 						
-						// Create Map
-						mapboxgl.accessToken = mapBoxKey;
-						var weatherMap = new mapboxgl.Map({
-							container: 'map',
-							style: 'mapbox://styles/mapbox/streets-v11',
-							zoom: 10,
-							center: [latitude, longitude]
-						})
+						
 						
 					}
 				})
