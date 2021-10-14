c26d709fc162ab72d8301b454c9d504dc7b426c4
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-13 21:20:01 -0500

    working on longest email reduce function

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -37,15 +37,21 @@ $(document).ready(function () {
 			yearsOfExperience: 9
 		}
 	];
-	
+	// Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
 	let hasOverThreeLangs = users.filter(user => user.languages.length > 2)
 	console.log(hasOverThreeLangs);
 	
+	// Use .map to create an array of strings where each element is a user's email address
 	let userEmails = users.map(user => `${user.name}'s email address is ${user.email}.`)
 	console.log(userEmails);
 	
+	// Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
 	let totalExperience = users.reduce((total, user) => {
 		return total + user.yearsOfExperience;
 	}, 0);
 	console.log(totalExperience + ' years of experience.');
+	
+	// Use .reduce to get the longest email from the list of users.
+	let longestEmail = users.reduce((emailLength, user) => (user.email.length > emailLength.length ? user: emailLength), '');
+	console.log(longestEmail);
 })
