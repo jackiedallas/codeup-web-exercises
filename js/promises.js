6f77b64555ec5bfd27fb3a94e697953c489e91e2
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-14 22:09:46 -0500

    extracted data from git hub promise and put it into an object

--- js/promises.js
+++ js/promises.js
@@ -1,13 +1,31 @@
 "use strict"
 $(document).ready(function () {
+	
+	// fetch('https://api.github.com/users/jackiedallas/events/public', {headers: {'Authorization': githubKey}})
+	// 	.then((response) => response.json())
+	// 	.then((userInfo) => userInfo.forEach((array) => console.log(array)))
 	fetch('https://api.github.com/users/jackiedallas/events/public', {headers: {'Authorization': githubKey}})
-		.then((response) => response.json())
-		// .then((data) => console.log(data))
-		.then((userInfo) => userInfo.forEach((array) => console.log(array.created_at)))
-		// .then((jsonData) => jsonData.results)
-		// .then((results) => console.log(results))
-		// .then((results) => results.forEach((result) => console.log(result.id)));
-		
-		
-		
+		.then(function (response) {
+			return response.json();
+		})
+		.then((userInfo) => {
+			const userObject = {}
+			let latestData = userInfo[0];
+			console.log(latestData)
+			var date = latestData.created_at;
+			var actorInfo = latestData.actor;
+			console.log(actorInfo)
+			var eventType = latestData.type;
+			console.log(eventType)
+			var repoInfo = latestData.repo;
+			
+			userObject.name = actorInfo.login;
+			userObject.lastPush = date;
+			userObject.gitHubURL = actorInfo.url;
+			userObject.repoName = repoInfo.name;
+			
+			console.log(userObject)
+			
+		})
+	
 })
