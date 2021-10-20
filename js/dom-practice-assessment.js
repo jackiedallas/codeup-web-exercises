2e1dcd58e4aa8e78261d36eeeb2266537467cb81
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-20 12:36:10 -0500

    finished problem 2

--- js/dom-practice-assessment.js
+++ js/dom-practice-assessment.js
@@ -1,6 +1,16 @@
 /**
  * Write your solutions here
  */
+
+// 1. When the box with the id of `dbl-click-box` is double-clicked, an alert should pop up with the text 'BOX DOUBLE-CLICKED!'
 $('.dbl-click-box').dblclick(function () {
 	alert('BOX DOUBLE-CLICKED!')
 })
+
+// 2. Clicking on the button with an id of `dark-mode-btn` toggles dark mode on and off. *HINT: changes should be applied to the body of the HTML.*
+$('#dark-mode-btn').click(function () {
+	$('body').toggleClass();
+})
+
+// 3. When the cursor hovers over the span with the id of uppercase (`<span id="uppercase">`), take the text of that span and display it, in lowercase, in the span with the id of lowercase (`<span id="lowercase">`).
+$('#uppercase')
